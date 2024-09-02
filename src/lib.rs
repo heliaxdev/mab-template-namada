a1b347f5010c102019167955dc21af5d74ac2bce
Tiago Carvalho <sugoiuguu@tfwno.gf>
2024-09-02 11:42:18 +0100

    generic debug

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
 use std::ffi::c_void;
 
-use namada_sdk::storage::BlockHeight;
-
 #[no_mangle]
 extern "C" fn mab_debug_impl(p: *mut c_void) {
-    let height: &BlockHeight = unsafe { &*(p as *const _) };
-    println!("{height:#?}");
+    debug::<namada_sdk::storage::BlockHeight>(p);
+}
+
+#[inline]
+fn debug<T: std::fmt::Debug>(p: *mut c_void) {
+    let p: &T = unsafe { &*(p as *const _) };
+    println!("{p:#?}");
 }
